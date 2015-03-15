36dbe60d5a18aa35289412dd15ee2f2ced671e23
kiran4399 <kiran4399@gmail.com>
2015-03-15 15:42:57 +0530

    modified firmware

--- src/pru-firmware/pru0_firmware.c
+++ src/pru-firmware/pru0_firmware.c
@@ -20,17 +20,17 @@ static void send_ret_value(int val)
 	//TODO : handle multiple params?
 	//TODO : change to sysfs
 	/*enable gloabl access*/
-	PRUCFG_SYSCFG = PRUCFG_SYSCFG & (~SYSCFG_STANDBY_INIT); 
+	PRUCFG_SYSCFG = PRUCFG_SYSCFG & (~SYSCFG_STANDBY_INIT);
 
 	/* write val to the loc pointed to by current ret_pointer */
 	*(shm_ret + ret_pointer) = val;
 
 	/*inc the ret_pointer to point to the next empty loc*/
-	ret_pointer = (ret_pointer % 1023) + 1; 
+	ret_pointer = (ret_pointer % 1023) + 1;
 	//max buffer space is 4096 => 1023 (free)words.
 
 	/*inc the num_ret_value*/
-	*shm_ret = ret_pointer; //ret_pointer is like a stack pointer, 
+	*shm_ret = ret_pointer; //ret_pointer is like a stack pointer,
 		//userspace will read this value to figure out till where the buffer has been filled
 		//e.g. if the ret_pointer = 10; ARM will read 9 slots uptill 10
 
@@ -42,7 +42,7 @@ static void send_ret_value(int val)
 static u32 get_second_word()
 {
 	u32 inst;
-	
+
 	/* when the 64 bit inst is part of the script */
 	if(!single_command){
 		PRUCFG_SYSCFG = PRUCFG_SYSCFG & (~SYSCFG_STANDBY_INIT); /*enable gloabl access*/
@@ -55,7 +55,7 @@ static u32 get_second_word()
 	else {
 		inst = single_command_2;
 	}
-	
+
 	return inst;
 }
 
@@ -66,8 +66,8 @@ int get_var_val(int addr)
 		if(addr < AIO_OFF){
 		//case DIO
 		//240 to 255 -> DIO[0] to DIO[15]
-			
-			addr -= 1; 
+
+			addr -= 1;
 			//this is there since if the code reaces here it is Arr[c/v]
 			//so either the userspace compiler or calling pru func here
 			//would have added 1. this should be taken care of.
@@ -91,15 +91,15 @@ void dio_handler(int opcode, u32 inst)
 	int val1, val2;
 	if(opcode == SET_DIO_a){
 	/* SET DIO[c/v], c/v */
-		
+
 		val1 = GET_BIT(inst, 23) ? var_loc[GET_BYTE(inst, 1)]: GET_BYTE(inst, 1);
 		val2 = GET_BIT(inst, 22) ? var_loc[GET_BYTE(inst, 0)]: GET_BYTE(inst, 0);
 	}
-	
-	else{	
+
+	else{
 		// "SET DIO[c], arr[v]"  orelse "SET DIO[v] , arr[v]"
 		val1 = (opcode == SET_DIO_b) ? GET_BYTE(inst, 2) : var_loc[GET_BYTE(inst,2)];
-		
+
 		//array size check -- this case same for both case
 		int index = var_loc[GET_BYTE(inst, 0)];
 		if (var_loc[GET_BYTE(inst,1)] <= index ){
@@ -113,16 +113,16 @@ void dio_handler(int opcode, u32 inst)
 		val2 = var_loc[addr];
 	}
 	PRUCFG_SYSCFG = PRUCFG_SYSCFG & (~SYSCFG_STANDBY_INIT); /*enable gloabl access*/
-	distribute(val1, GPIO1_INSTANCE_ADDRESS);
+	init(val1, GPIO1_INSTANCE_ADDRESS);
 	/* set hi*/
-	if(val2 && (val1 < MAX_DIO)){ 
+	if(val2 && (val1 < MAX_DIO)){
         	//__R30 = __R30 | ( 1 << val1);   //original line
-		GPIOPinWrite(GPIO1_INSTANCE_ADDRESS, val1, GPIO_PIN_HIGH);		        
+		GPIOPinWrite(GPIO1_INSTANCE_ADDRESS, val1, GPIO_PIN_HIGH);
 }
 
 	/* set low*/
-        else{ 
-		GPIOPinWrite(GPIO1_INSTANCE_ADDRESS, val1, GPIO_PIN_LOW);		        
+        else{
+		GPIOPinWrite(GPIO1_INSTANCE_ADDRESS, val1, GPIO_PIN_LOW);
         	//__R30 = __R30 & ~( 1 << val1);  //original line
         }
 	int j;
@@ -196,8 +196,8 @@ void set_handler(int opcode, u32 inst)
 		int op = GET_BYTE(inst, 2) >> 6;
 
 		//if op == 1, operand1 is a variable; else it is a Arr[v] type
-		if (op == 1){ 
-			addr1 = GET_BYTE(inst, 0); 
+		if (op == 1){
+			addr1 = GET_BYTE(inst, 0);
 		}
 
 		//Arr[v]
@@ -205,11 +205,11 @@ void set_handler(int opcode, u32 inst)
 			//array size check
 	                int index = var_loc[GET_BYTE(inst, 0)];
                 	if (var_loc[GET_BYTE(inst,1)] <= index ){
-                        	//error 
+                        	//error
                         	if (single_command)
                                 	send_ret_value(0);
                         	return;
-                	} 
+                	}
 			addr1 = GET_BYTE(inst,1) + index + 1;
 		}
 
@@ -217,9 +217,9 @@ void set_handler(int opcode, u32 inst)
 
 		//get the next 32 bits of the inst
 		inst = get_second_word();
-		
+
 		op = ((GET_BYTE(old_inst, 2) >> 4) & 3); //get bits 4 and 5 of byte2
-		
+
 		if (op == 0){
 		//operand2 is of type C
 			val2 = inst & 0xFF;
@@ -243,7 +243,7 @@ void set_handler(int opcode, u32 inst)
 			//array size check
 			int index = var_loc[GET_BYTE(inst, 0)];
                         if (var_loc[GET_BYTE(inst,1)] <= index ){
-                                //error 
+                                //error
                                 if (single_command)
                                         send_ret_value(0);
                                 return;
@@ -285,7 +285,7 @@ void wait_goto_get_handler(int opcode, u32 inst)
                 //array size check
                 int index = var_loc[GET_BYTE(inst, 0)];
                 if (var_loc[GET_BYTE(inst,1)] <= index ){
-                    //error 
+                    //error
                         if (single_command)
 				send_ret_value(0);
                         return;
@@ -293,7 +293,7 @@ void wait_goto_get_handler(int opcode, u32 inst)
 
 		val = get_var_val(GET_BYTE(inst,1) + index + 1);
 	}
-	
+
 	if(opcode == WAIT){
 		wait(val);
 	}
@@ -302,13 +302,13 @@ void wait_goto_get_handler(int opcode, u32 inst)
 		inst_pointer = val;
 
 	else if((opcode == GET) && single_command)
-		send_ret_value(val);	
+		send_ret_value(val);
 }
 
 #if 1
 void if_handler(int opcode, u32 inst)
 {
-	/* IF (a <cond> b) GOTO c 
+	/* IF (a <cond> b) GOTO c
 		a => C/V/Arr[V] (byte1, byte0)
 		b => C/V/Arr[V] (byte5, byte4)
 		c => C/V/Arr[V] (byte7, byte6)
@@ -316,7 +316,7 @@ void if_handler(int opcode, u32 inst)
 	int val1, val2, val3;
 	int op = GET_BYTE(inst, 2) >> 6;
 	int rel_op, res;
-	
+
 	rel_op = opcode & 0x0F;
 
 	if(op == 0){
@@ -335,7 +335,7 @@ void if_handler(int opcode, u32 inst)
 	        //array size check
 	        int index = var_loc[GET_BYTE(inst, 0)];
 	        if (var_loc[GET_BYTE(inst,1)] <= index ){
-	        	//error 
+	        	//error
 	        	if (single_command)
 	                	send_ret_value(0);
 	        	return;
@@ -345,10 +345,10 @@ void if_handler(int opcode, u32 inst)
 	}
 
 	u32 old_inst = inst; //old_inst is the 1st 32 bits of the inst.
-	
+
 	//get the next 32 bits of the inst
 	inst = get_second_word();
-	
+
 	/*** GET val2 from second part of the inst ***/
 
         op = (GET_BYTE(old_inst, 2) >> 4) & 3;
@@ -369,7 +369,7 @@ void if_handler(int opcode, u32 inst)
                 //array size check
                 int index = var_loc[GET_BYTE(inst, 0)];
                 if (var_loc[GET_BYTE(inst,1)] <= index ){
-                        //error 
+                        //error
                         if (single_command)
                                 send_ret_value(0);
                         return;
@@ -398,7 +398,7 @@ void if_handler(int opcode, u32 inst)
                 //array size check
                 int index = var_loc[GET_BYTE(inst, 2)];
                 if (var_loc[GET_BYTE(inst,3)] <= index ){
-                        //error 
+                        //error
                         if (single_command)
                                 send_ret_value(0);
                         return;
@@ -406,8 +406,8 @@ void if_handler(int opcode, u32 inst)
 
                 val3 = get_var_val(GET_BYTE(inst,3) + index + 1);
         }
-	
-	/* calculate the result based on rel_op 
+
+	/* calculate the result based on rel_op
 		==	0
 		!=	1
 		>=	2
@@ -461,68 +461,68 @@ void math_handler(int opcode, u32 inst)
 		//val1 = GET_BIT(inst, 23) ? var_loc[GET_BYTE(inst, 1)]: GET_BYTE(inst, 1);
 		addr = GET_BYTE(inst,1);
 		val1 = var_loc[addr]; //val1 can't be const
-		val2 = GET_BIT(inst, 22) ? var_loc[GET_BYTE(inst, 0)]: GET_BYTE(inst, 0);	
+		val2 = GET_BIT(inst, 22) ? var_loc[GET_BYTE(inst, 0)]: GET_BYTE(inst, 0);
 	}
-	
+
 	else{
 	/* 64 bit inst */
-		//TODO the following should be abstracted away into a fn 
+		//TODO the following should be abstracted away into a fn
 		//since all 64 bit inst have the same operator placement, common fn to extract all values
 
 		int op = GET_BYTE(inst, 2) >> 6;
 
-		if(op == 0){//XXX : this case is not possible 
+		if(op == 0){//XXX : this case is not possible
         	/* operand1 is of type C */
                 	val1 = GET_BYTE(inst, 0);
         	}
-	
+
 	        else if(op == 1){
 	        /* operand1 is of type V */
 			addr = GET_BYTE(inst,0);
 	                val1 = get_var_val(addr);
 	        }
-	
+
 	        else{
 	        /* operand1 is of type Arr[v] */
 
 	                //array size check
 	                int index = var_loc[GET_BYTE(inst, 0)];
 	                if (var_loc[GET_BYTE(inst,1)] <= index ){
-	                        //error 
+	                        //error
 	                        if (single_command)
 	                                send_ret_value(0);
 	                        return;
 	                }
 
         	        val1 = get_var_val(GET_BYTE(inst,1) + index + 1);
-	        }	
-		
+	        }
+
 		u32 old_inst = inst; //old_inst is the 1st 32 bits of the inst.
 
 	        //get the next 32 bits of the inst
 	        inst = get_second_word();
-	
+
 	        /*** GET val2 from second part of the inst ***/
-	
+
 	        op = (GET_BYTE(old_inst, 2) >> 4) & 3;
-	
+
        		if(op == 0){
 	        /* operand2 is of type C */
 	                val2 = GET_BYTE(inst, 0);
 	        }
-	
+
 	        else if(op == 1){
 	        /* operand2 is of type V */
 	                val2 = get_var_val(GET_BYTE(inst, 0));
 	        }
-	
+
 	        else{
 	        /* operand2 is of type Arr[v] */
 
 	                //array size check
 	                int index = var_loc[GET_BYTE(inst, 0)];
 	                if (var_loc[GET_BYTE(inst,1)] <= index ){
-	                        //error 
+	                        //error
 	                        if (single_command)
 	                                send_ret_value(0);
 	                        return;
@@ -531,7 +531,7 @@ void math_handler(int opcode, u32 inst)
 	                val2 = get_var_val(GET_BYTE(inst,1) + index + 1);
 	        }
 	}
-	
+
 	switch(opcode){
 		case ADD_32:
 		case ADD_64:
@@ -542,7 +542,7 @@ void math_handler(int opcode, u32 inst)
 		case SUB_64:
 			res = val1 - val2;
 		break;
-		
+
 		case AND_32:
 		case AND_64:
 			res = val1 & val2;
@@ -597,7 +597,7 @@ static int handle_downcall(u32 id, u32 arg0, u32 arg1, u32 arg2,
 			*shm_ret = 1;
 			PRUCFG_SYSCFG = PRUCFG_SYSCFG | SYSCFG_STANDBY_INIT;
 		break;
-		
+
 		case SYS_EXEC:
 			/*
 				arg0 == 0 : pause
@@ -628,19 +628,19 @@ static int handle_downcall(u32 id, u32 arg0, u32 arg1, u32 arg2,
 		/* returns the status of execution i.e. executing or not */
 			return (is_executing | is_waiting);
 		break;
-		
+
 		case SYS_INST_32:
 		/* an single 32 bit instruction has been recieved for execution */
 			single_command = (u32)arg0;
 		break;
-		
+
 		case SYS_INST_64:
 		/* an single 32 bit instruction has been recieved for execution */
 			single_command = (u32)arg0;
 			single_command_2 = (u32)arg1; //2nd word for 64 bit inst
 			//bad naming convention - alexanderhiam is not going to be happy
 		break;
-		
+
 		default:
 			return 0; /* error case - unknown instruction in memory */
 		break;
@@ -659,7 +659,7 @@ void check_event(void)
 		/* call the handler to perform the downcall*/
 		sc_downcall(handle_downcall);
 	}
-	
+
 
 	if(PIEP_CMP_STATUS & 1){
 		/* disable the timer */
@@ -670,7 +670,7 @@ void check_event(void)
 
 		/* set CMP0 value back to zero */
 		PIEP_CMP_CMP0 = 0;
-		
+
 		/* disable events from CMP0*/
        		PIEP_CMP_CFG &= ~(CMP_CFG_CMP_EN(0));
 
@@ -688,7 +688,7 @@ void check_event(void)
 }
 
 void wait(int ms)
-{	
+{
 	/* set the CMP0 reg value */
 	//PIEP_CMP_CMP0 =  MS * ms;
 
@@ -706,7 +706,7 @@ void wait(int ms)
 	PIEP_CMP_STATUS = CMD_STATUS_CMP_HIT(0);
 
         /* start the timer */
-        //PIEP_GLOBAL_CFG |= GLOBAL_CFG_CNT_ENABLE;	
+        //PIEP_GLOBAL_CFG |= GLOBAL_CFG_CNT_ENABLE;
 }
 
 void execute_instruction()
@@ -721,8 +721,8 @@ void execute_instruction()
 	else {
 		inst = single_command;
 	}
-	
-	int opcode = inst >> 24; //most significant byte contains the cmd 
+
+	int opcode = inst >> 24; //most significant byte contains the cmd
 
 	switch(opcode){
 
@@ -731,7 +731,7 @@ void execute_instruction()
 		case SET_DIO_c:
 			dio_handler(opcode, inst);
 		break;
-	
+
 		case SET_PWM_a:
 			pwm_handler(opcode, inst);
 		break;
@@ -741,11 +741,11 @@ void execute_instruction()
 		case SET_64:
 			set_handler(opcode, inst);
 		break;
-		
+
 		case SET_ARR_DEC:
 			array_dec_handler(opcode, inst);
 		break;
-	
+
 		case WAIT:
 		case GOTO:
 		case GET:
@@ -760,7 +760,7 @@ void execute_instruction()
 		case IF_LT:
 			if_handler(opcode, inst);
 		break;
-		
+
 		case ADD_32:
 		case ADD_64:
 		case SUB_32:
@@ -771,7 +771,7 @@ void execute_instruction()
 		case OR_64:
 			math_handler(opcode, inst);
 		break;
-		
+
 		case HALT:
 			is_waiting = 0;
 			is_executing = 0;
@@ -783,7 +783,7 @@ void execute_instruction()
 		break;
 
 	}
-	
+
 	single_command = 0; /*incase there was a single command executed this time, set to zero*/
 }
 
@@ -794,7 +794,7 @@ void timer_init()
 
         /* Compare registers enable, enables signal from CMP0, on CMP0 --> counter is reset */
         //PIEP_CMP_CFG |=  RESET_ON_CMP0_EVENT; -- cant afford to do this when timer is shared
-	
+
 	/* clear the counter status bit for CMP0 register, i.e. PIEP_CMP_STATUS[0] */
         PIEP_CMP_STATUS = CMD_STATUS_CMP_HIT(0); /* clear the interrupt */
 
@@ -804,19 +804,19 @@ void timer_init()
 
 int main()
 {
-	
+
 	timer_init();
 	//send_ret_value(1000);
 	while(1)
 	{
 		check_event();
-		
+
 		if (is_executing || single_command){ //or if single_cmd
 			execute_instruction();
 			//send_ret_value(is_executing);
 			//send_ret_value(single_command);
 		}
-		
+
 	}
 
 	return 0;
